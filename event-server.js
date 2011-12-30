9687ecfa77f04fa45a13994ecad83839c5ca5f95
iwater <iwater@gmail.com>
2011-12-30 14:33:37 +0800

    change listen address

--- event-server.js
+++ event-server.js
@@ -2,8 +2,8 @@ var zmq = require('zmq')
   , sub = zmq.socket('sub')
   , pub = zmq.socket('pub');
 
-pub.bindSync('tcp://127.0.0.1:3001');
-sub.bindSync('tcp://127.0.0.1:3002');
+pub.bindSync('tcp://0.0.0.0:3001');
+sub.bindSync('tcp://0.0.0.0:3002');
 
 sub.on('message', function(msg){
   console.log(msg.toString());
