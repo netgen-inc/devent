c764c18064bab00f0a194f98981f64ef88c8e2c9
iwater <iwater@gmail.com>
2012-01-09 14:20:56 +0800

    add args for server start --pub --sub --debug

--- event-server.js
+++ event-server.js
@@ -1,12 +1,17 @@
+var argv = require('optimist').argv;
 var zmq = require('zmq')
   , sub = zmq.socket('sub')
   , pub = zmq.socket('pub');
 
-pub.bindSync('tcp://0.0.0.0:3001');
-sub.bindSync('tcp://0.0.0.0:3002');
+var debug = argv.debug || false;
+
+pub.bindSync(argv.pub || 'tcp://0.0.0.0:3001');
+sub.bindSync(argv.sub || 'tcp://0.0.0.0:3002');
 
 sub.on('message', function(msg){
-  console.log(msg.toString());
   pub.send(msg);
+  if( debug ) {
+    console.log(msg.toString());
+  }
 });
 sub.subscribe('');
